e048dd61084c78c76b27aebae475056e9ed812fc
Gregwar <g.passault@gmail.com>
2012-08-13 14:50:56 +0200

    Not using sendMulti()

--- web/setcenter.php
+++ web/setcenter.php
@@ -4,9 +4,7 @@ $context = new ZMQContext(1);
 $client = new ZMQSocket($context, ZMQ::SOCKET_REQ);
 $client->connect('tcp://localhost:8787');
 
-$client->sendMulti(array(
-    'setCenter',
-    sprintf('%g %g', $_GET['x'], $_GET['y'])
-));
+$client->send('setCenter', ZMQ::MODE_SNDMORE);
+$client->send(sprintf('%g %g', $_GET['x'], $_GET['y']));
 
 echo $client->recv();
